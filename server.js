affafc5b22af13b8108524e7556c72390949208c
DanielMalede <danielmalede@gmail.com>
2022-11-15 21:18:42 +0200

    .

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 3000;
-const mainRouter = require('./routes/main')
+const mainRouter = require("./routes/main");
 app.use(cors());
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(mainRouter)
+app.use(mainRouter);
 
-app.use((req,res)=>{
-  res.status(404).send('not found')
-})
+app.use((req, res) => {
+  res.status(404).send("not found");
+});
 
 app.listen(port, () => {
   console.log(`server is ${port}`);
